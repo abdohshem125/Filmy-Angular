9d50a77f5e059c7f8e9f292d06770deeb6184062
Abdulrahman Hashem <abdohashim228@gmail.com>
2025-10-10 18:03:40 +0300

    Initial Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Movies } from './components/movies/movies';
+import { MovieDetails } from './components/movie-details/movie-details';
+import { Profile } from './components/profile/profile';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { ContactUs } from './components/contact-us/contact-us';
+import { AboutUs } from './components/about-us/about-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'movies', component: Movies },
+  { path: 'movieDetails', component: MovieDetails },
+  { path: 'profile', component: Profile },
+  { path: 'contactUs', component: ContactUs },
+  { path: 'aboutUs', component: AboutUs },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
