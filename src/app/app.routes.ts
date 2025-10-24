0049944d684fd84885c3e2e38225f0302c78de04
abdohshem125 <120470147+abdohshem125@users.noreply.github.com>
2025-10-24 23:24:39 +0300

    contact us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { HomeComponent } from './components/home/home.component';
 import { Movies } from './components/movies/movies';
 import { MovieDetails } from './components/movie-details/movie-details';
 import { Profile } from './components/profile/profile';
-import { ContactUs } from './components/contact-us/contact-us';
+// import { ContactUs } from './components/contact-us/contact-us';
 // import { AboutUs } from './components/about-us/about-us';
+import { ContactUsComponent } from './components/contact-us/contact-us';
 import { AboutUsComponent } from './components/about-us/about-us';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
@@ -17,7 +18,7 @@ export const routes: Routes = [
   { path: 'movies/:genre', component: Movies },
   { path: 'movieDetails/:id', component: MovieDetails },
   { path: 'profile', component: Profile },
-  { path: 'contactUs', component: ContactUs },
+  { path: 'contactUs', component: ContactUsComponent },
   { path: 'aboutUs', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
--- src/app/components/contact-us/contact-us.ts
+++ src/app/components/contact-us/contact-us.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-us.html',
-  styleUrl: './contact-us.css'
+  styleUrls: ['./contact-us.css']
 })
-export class ContactUs {
+export class ContactUsComponent {
+  name = '';
+  email = '';
+  message = '';
 
+  onSubmit() {
+    if (this.name && this.email && this.message) {
+      alert(Thank you, ${this.name}! Your message has been sent.);
+      this.name = '';
+      this.email = '';
+      this.message = '';
+    } else {
+      alert('Please fill in all fields.');
+    }
+  }
 }
