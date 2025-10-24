56f30e69523518a953525fcc5ae7feef426658fc
abdohshem125 <120470147+abdohshem125@users.noreply.github.com>
2025-10-24 23:18:59 +0300

    update about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { Movies } from './components/movies/movies';
 import { MovieDetails } from './components/movie-details/movie-details';
 import { Profile } from './components/profile/profile';
 import { ContactUs } from './components/contact-us/contact-us';
-import { AboutUs } from './components/about-us/about-us';
+// import { AboutUs } from './components/about-us/about-us';
+import { AboutUsComponent } from './components/about-us/about-us';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { authGuard } from './services/auth.guard';
@@ -17,7 +18,7 @@ export const routes: Routes = [
   { path: 'movieDetails/:id', component: MovieDetails },
   { path: 'profile', component: Profile },
   { path: 'contactUs', component: ContactUs },
-  { path: 'aboutUs', component: AboutUs },
+  { path: 'aboutUs', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
 ];
